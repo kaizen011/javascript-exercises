ab85f3c32e0ef18d376c11eed27549e3e31b54fa
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-04-05 22:03:30 +0200

    Finished until the 4th exercise barely surpassed the tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenElements, ...removeable) {
+    const emptyArray =[];
+    
+    givenElements.forEach(element => {   
+        if(!removeable.includes(element)){
+            emptyArray.push(element);
+        }
+    });
+    return emptyArray;
 };
 
 // Do not edit below this line
