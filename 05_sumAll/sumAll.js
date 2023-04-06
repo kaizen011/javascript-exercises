ddccd637ab3b4036c73555848470ea86fe8cd6b7
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-04-06 10:54:15 +0200

    Finished exercises (0-6)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let x = a;
+    let sum = 0;
+    for(x; x <= b; x++){
+        sum += x;
+    }
+    return sum;
 };
 
 // Do not edit below this line
