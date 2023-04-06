1528a52307aef677029f1ed9cd27a46fc0029fa2
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-04-06 11:17:57 +0200

    Completed exercises [1-7]

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  let cels = Math.round((number - 32) * (5/9) * 10) / 10;
+  return cels;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number1) {
+  let fahr = Math.round(((number1 * 9) / 5 + 32) *10 )/10;
+  return fahr;
 };
 
 // Do not edit below this line
