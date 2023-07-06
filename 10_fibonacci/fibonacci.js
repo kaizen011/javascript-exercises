8fed48e6c1970f21570346bf5593892caefedcfe
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-07-06 23:07:20 +0200

    Finished these exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+/*
 
+*/
+const fibonacci = function(n) {
+    if (n === 0) {
+        return 0;
+      } else if (n === 1) {
+        return 1;
+      } else {
+        let previous = 0;
+        let current = 1;
+    
+        for (let i = 2; i <= n; i++) {
+          let next = previous + current;
+          previous = current;
+          current = next;
+        }
+    
+        return current;
+      }
+    
 };
 
 // Do not edit below this line
